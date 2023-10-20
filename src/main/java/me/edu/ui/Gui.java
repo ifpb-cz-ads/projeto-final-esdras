9984ce1c7104f724cc36fa7193d6e6f254aba26f
Esdras Silva <esdraslimasilva83@gmail.com>
2023-10-20 14:37:46 -0300

    Create more concise UI

--- src/main/java/me/edu/ui/Gui.java
+++ src/main/java/me/edu/ui/Gui.java
@@ -1,6 +1,7 @@
 package me.edu.ui;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
@@ -9,6 +10,9 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.UIManager;
+import javax.swing.plaf.basic.BasicScrollBarUI;
 
 public class Gui {
     private static final int WINDOW_WIDTH = 800;
@@ -19,6 +23,33 @@ public class Gui {
     private static final Color RED = new Color(193, 60, 60);
 
     private Gui() {
+    }
+
+    private static JPanel createHeaderSection() {
+        JPanel headerSection = new JPanel();
+        headerSection.add(new JLabel("Header Section"));
+        headerSection.setOpaque(true);
+        headerSection.setBackground(GREEN);
+
+        return headerSection;
+    }
+
+    private static JPanel createDBsSection() {
+        JPanel dbsSection = new JPanel();
+        dbsSection.add(new JLabel("DBs Section"));
+        dbsSection.setOpaque(true);
+        dbsSection.setBackground(GREEN);
+
+        return dbsSection;
+    }
+
+    private static JPanel createQueryToolSection() {
+        JPanel queryToolSection = new JPanel();
+        queryToolSection.add(new JLabel("Query Tool Section"));
+        queryToolSection.setOpaque(true);
+        queryToolSection.setBackground(GREEN);
+
+        return queryToolSection;
     }
 
     public static void init() {
@@ -37,34 +68,64 @@ public class Gui {
 
         gbc.fill = GridBagConstraints.BOTH;
 
-        JPanel headerSection = new JPanel();
-        headerSection.add(new JLabel("Header Section"));
-        headerSection.setOpaque(true);
-        headerSection.setBackground(GREEN);
+        // creating and setting up header section
+        JPanel headerSection = createHeaderSection();
         gbc.gridx = 0;
         gbc.gridy = 0;
+        gbc.ipady = 70;
         mainPanel.add(headerSection, gbc);
 
-        JPanel dbsSection = new JPanel();
-        dbsSection.add(new JLabel("DBs Section"));
-        dbsSection.setOpaque(true);
-        dbsSection.setBackground(GREEN);
+        // creating and setting up databases section
+        JPanel dbsSection = createDBsSection();
         gbc.gridx = 0;
         gbc.gridy = 1;
+        gbc.ipady = 400;
         mainPanel.add(dbsSection, gbc);
 
-        JPanel queryToolSection = new JPanel();
-        queryToolSection.add(new JLabel("Query Tool Section"));
-        queryToolSection.setOpaque(true);
-        queryToolSection.setBackground(GREEN);
+        // creating and setting up query tool section
+        JPanel queryToolSection = createQueryToolSection();
         gbc.gridx = 0;
         gbc.gridy = 2;
         mainPanel.add(queryToolSection, gbc);
 
-        windowFrame.add(mainPanel);
+        // Create a JScrollPane to make the window scrollable
+        JScrollPane scrollPane = new JScrollPane(mainPanel);
+
+        windowFrame.add(scrollPane);
+
+        // customizing scrollbar
+        scrollPane.getVerticalScrollBar().setUI(new BasicScrollBarUI() {
+            @Override
+            protected void configureScrollBarColors() {
+                this.thumbColor = GREEN;
+                this.trackColor = LIGHT_GRAY;
+
+            }
+
+            // removing the scrollbar buttons
+            @Override
+            protected JButton createDecreaseButton(int orientation) {
+                JButton increaseScrollbarButton = new JButton();
+                increaseScrollbarButton.setPreferredSize(new Dimension(0, 0));
+                increaseScrollbarButton.setMinimumSize(new Dimension(0, 0));
+                increaseScrollbarButton.setMaximumSize(new Dimension(0, 0));
+                return increaseScrollbarButton;
+            }
+
+            @Override
+            protected JButton createIncreaseButton(int orientation) {
+                JButton decreaseScrollbarButton = new JButton();
+                decreaseScrollbarButton.setPreferredSize(new Dimension(0, 0));
+                decreaseScrollbarButton.setMinimumSize(new Dimension(0, 0));
+                decreaseScrollbarButton.setMaximumSize(new Dimension(0, 0));
+                return decreaseScrollbarButton; // Create a dummy button for the bottom arrow
+            }
+
+        });
 
         windowFrame.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
         windowFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        windowFrame.setResizable(false);
         windowFrame.setVisible(true);
     }
 };
